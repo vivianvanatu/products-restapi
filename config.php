2dfca265548c89999380e8cdbbe33814ada65e2d
Vivian.Vanatu <vivian.vanatu@iris.co.uk>
2024-01-31 14:11:39 +0200

    more config changes

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-	class Config {
-		public $dbhost = 'localhost';
-		public $dbuser = 'products-restapi';
-		public $dbpass = 'KBk9CuNj4vK4bPUULlqemF';
-		public $dbname = 'products-restapi';
-	}
-?>
+class Config
+{
+	public $dbhost = 'localhost';
+	public $dbuser = 'productsrestapi';
+	public $dbpass = 'KBk9CuNj4vK4bPUULlqemF';
+	public $dbname = 'productsrestapi';
+}
--- init.sql
+++ init.sql
@@ -1,15 +1,15 @@
 -- Create a user and set password
-CREATE USER 'products-restapi'@'%' IDENTIFIED BY 'KBk9CuNj4vK4bPUULlqemF';
+CREATE USER 'productsrestapi'@'%' IDENTIFIED BY 'KBk9CuNj4vK4bPUULlqemF';
 
 -- Grant privileges to the user for the specified database
-GRANT ALL PRIVILEGES ON products-restapi.* TO 'products-restapi'@'%';
+GRANT ALL PRIVILEGES ON productsrestapi.* TO 'productsrestapi'@'%';
 
 -- Flush privileges to apply the changes
 FLUSH PRIVILEGES;
 
 -- Create the database and switch to it
-CREATE DATABASE IF NOT EXISTS products-restapi;
-USE products-restapi;
+CREATE DATABASE IF NOT EXISTS productsrestapi;
+USE productsrestapi;
 
 -- Load data from products.sql
 source /docker-entrypoint-initdb.d/products.sql;
